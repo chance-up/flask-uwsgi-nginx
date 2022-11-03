130773da1097eac9d0ba3d03c4b39479db52c91e
Chansub Choi <ccs6473@naver.com>
2022-11-03 13:34:25 +0900

    front/back proxy

--- backend/app.py
+++ backend/app.py
@@ -5,10 +5,14 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
   
-@app.route('/')
+@app.route('/api')
 def index():
     return "Hello World!"
 
+@app.route('/api/a')
+def index1():
+    return "Hello World! a"
+
 
 if __name__ == "__main__": 
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
